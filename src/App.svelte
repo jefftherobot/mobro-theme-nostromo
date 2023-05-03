53e86275d69cb5729be84a46443a52ef95abc9a4
Jeff <he.man@skele.to>
2023-05-03 11:15:49 -0400

    started theme mockup

---
+++ src/App.svelte
@@ -0,0 +1,68 @@
+<script>
+	import CPU from './components/CPU.svelte'
+	import GPU from './components/GPU.svelte'
+</script>
+
+<main>
+	<div class="title-row">
+		<h1>Personal Terminal</h1>
+		<hr/>
+	</div>
+		
+	<div class="block loads-block">
+		<h2>loads</h2>
+		<CPU/>
+		<GPU/>
+	</div>
+	<div class="block">
+		<h2>thermals</h2>
+	</div>
+	<div class="block">
+		<h2>fans</h2>
+	</div>
+	<div class="block">
+		<h2>clocks</h2>
+	</div>
+	<div class="block">
+		<h2>power</h2>
+	</div>
+	<div class="block">
+		<h2>ram</h2>
+	</div>
+
+</main>
+
+
+
+<style>
+
+	main {
+		display: grid;
+  	grid-template-columns: repeat(3, 1fr);
+		grid-template-rows: auto repeat(2, 1fr);
+		text-align: center;
+		padding: 0;
+		margin: 0 auto;
+		height: 100%;
+		width:100% ;
+	}
+
+	h1 {
+		margin: 0;
+		color: #000;
+		text-transform: uppercase;
+		font-size: 2em;
+		line-height: .8;
+		font-weight: 100;
+		background: yellow;
+	}
+
+	.title-row{
+		margin: 0 0 10px;
+		grid-column: span 3;
+	}
+
+	.block{
+		/* border: 1px solid yellow */
+	}
+</style>
---
+++ src/components/GPU.svelte
@@ -0,0 +1,98 @@
+<script>
+  import { Bar } from 'svelte-chartjs';
+  import {
+    Chart,
+    BarElement,
+    CategoryScale,
+    LinearScale,
+  } from 'chart.js';
+
+  Chart.register(
+    BarElement,
+    CategoryScale,
+    LinearScale
+  );
+
+  let clear
+  let chartRef
+  let ms = 2000
+  let load = [20, 100]
+
+  let data = {
+    labels: ['CPU'],
+    datasets: [
+      {
+        label: '%',
+        data: load,
+      },
+    ],
+  };
+
+
+  const random = (min, max) => Math.floor(Math.random() * (max - min)) + min;
+  const incr = () => (load[0] = random(10,20),chartRef.update() )
+
+
+  $: {
+    clearInterval(clear)
+    clear = setInterval(incr, ms)
+  }
+
+  
+// console.log(MobroSDK)
+// MobroSDK.init()
+// MobroSDK.addChannelListener("general_processor_usage", (data) => {
+//  //console.log('test',data.payload)
+//     if(data.payload){
+//         load[0]=Math.round(data.payload.value);
+//         chartRef.update()
+//     }else{
+//        load[0]=0
+//        chartRef.update()
+//     }
+// })
+</script>
+
+<div class="label">
+  <!-- <span>{data.datasets[0].data[0]}% </span> -->
+  <span>{load[0]}%</span>
+  <span>gpu</span>
+</div>
+
+<hr/>
+
+<div class="chart-container">
+  <Bar 
+    bind:chart={chartRef} 
+    {data}
+    options={{
+      backgroundColor:"yellow", 
+      indexAxis: 'y',
+      scales: { 
+        y:{ display:false},
+        x:{ display:false}
+      },
+      responsive: true,
+      maintainAspectRatio: false }} />
+</div>
+
+<style>
+  .label {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    padding: 4px;
+    text-align: left;
+    font-size: 1em;
+   
+  }
+
+  .label>span:nth-child(2){
+    text-align: right;
+  } 
+
+  .chart-container {
+    width: 100px;
+    height: 20px;
+  }
+
+</style>
